64b5c5c40a93fbd6ce3125f4f203191bf29509c6
Nick Tolimieri <nick.tolimieri@noaa.gov>
2022-03-28 16:09:19 -0700

    add understory kelps

--- Flagstone paper/RMarkdowns/fish_kelp_binomial.r
+++ Flagstone paper/RMarkdowns/fish_kelp_binomial.r
@@ -23,7 +23,6 @@ Fig_Loc = paste0(HomeFile,"/Plots/")
 Data_Loc = paste0(HomeFile,"/Data/")
 Results_Loc = paste0(HomeFile,"/Results/")
 Other_Files = paste0(HomeFile,"/Other Files/")
-
 theme_nt = readRDS(paste0(Data_Loc, 'theme_nt.rds') )
 
 settings = readRDS(paste0(Data_Loc,'settings.rds') )
@@ -44,14 +43,17 @@ kelp_codes = data.frame(read.csv( paste0(Data_Loc,"spp_codes_kelp.csv") ))
 
 # MEAN DENSITY ####
 df_dens = readRDS( paste0(Data_Loc,"Data_Fish_Kelp_area_wide.rds"))
-df_dens <- df_dens %>% rename(Macro= MACPYR , Nereo=NERLUE, Ptery=PTECAL)
+df_dens <- df_dens %>% rename(Macro= MACPYR , Nereo=NERLUE, Ptery=PTECAL) %>%
+        mutate(understory = sum(AGAFIM+ALAMAR+COSCOS+CYMTRI+DESSPP+LAMSET+SACGRO+SACLAT+PLEGAR ))
 df_dens$area[df_dens$year==2015] <- "D"
 
 # TOTALCOUNT ####
 df_count <- readRDS(paste0(Data_Loc,"Fish-kelp-counts-wide.rds"))
 df_count <- df_count %>% 
         rename(Macro= MACPYR , Nereo=NERLUE, Ptery=PTECAL) %>%
-        mutate(fish_volume = fish_area*2)
+        mutate(fish_volume = fish_area*2,
+               understory = sum(AGAFIM+ALAMAR+COSCOS+CYMTRI+DESSPP+LAMSET+SACGRO+SACLAT+PLEGAR ))
+
 
 # add the area informatinto the df_dens file
 # has area data for weighting
@@ -84,22 +86,30 @@ ptery1 <- ggplot( df_dens , aes(x = Ptery, y = TOTyoy, color=site)) +
      scale_color_manual(values = site.col$col) +
      theme_bw() + theme_nt
 
+under1 <- ggplot( df_dens , aes(x = understory, y = TOTyoy, color=site)) +
+        geom_point() + 
+        xlab( expression(paste(italic('Understory'), ' stipes per ', m^2)) )+
+        ylab( expression(paste('Rockfish YOY per 60 ',m^2) )) + 
+        scale_color_manual(values = site.col$col) +
+        theme_bw() + theme_nt
+
 macro1 <- macro1 + theme(legend.position = c(0.75,0.8))
 nereo1 <- nereo1 + theme(legend.position = 'none')
 ptery1 <- ptery1 + theme(legend.position = 'none')
+under1 <-under1 + theme(legend.position = 'none')
      
 par(ps = 10, cex =1)
 
 graphics.off()
 
 png(paste0(Fig_Loc,"Univariate-YOY-v-Kelp.simple.png"), units = 'in', res=300, height=5, width=3)
-ggarrange(macro1, nereo1, ptery1,
-          labels = c('a)','b)','c)'), 
+ggarrange(macro1, nereo1, ptery1, under1,
+          labels = c('a)','b)','c)', 'd)'), 
           font.label = list(face='plain', size = 10),
           hjust = -5.5,
           vjust = 2.5,
           align = 'v',
-          nrow = 3, ncol=1)
+          nrow = 4, ncol=1)
 dev.off()
 
 
@@ -114,6 +124,7 @@ dfx <- df_dens
 dfx <- dfx %>% mutate(TOTyoy_pres = ifelse(TOTyoy > 0 , 1, 0)) %>%
                mutate(three_kelps = Macro+Nereo+Ptery , 
                     canopy_kelp = Macro+Nereo,
+                    under2 = Ptery+understory,
                     transect.area.algae.weight = kelp_area/max(kelp_area),
                     fish.area.weight = fish_area/max(fish_area),
                     n = 1)
@@ -130,99 +141,56 @@ sum(dfx$TOTyoy_pres)/sum(dfx$n)
 ######## Begin occurrence models ##########################################
 ###########################################################################
 
-m_year <- glmer( TOTyoy_pres ~  (1|year_factor), 
-                  family = binomial, 
-                  weights = fish.area.weight,
-                  data = dfx)
-
-m_rand <- glmer( TOTyoy_pres ~  (1|year_factor)+ (1|site), 
+m_MNPU <- glmer( TOTyoy_pres ~  Macro+Nereo+Ptery+understory + (1|year_factor) + (1|site), 
+                family = binomial, 
+                weights = fish.area.weight,
+                na.action = 'na.fail',
+                data = dfx)
+m_CU <- glmer( TOTyoy_pres ~  canopy_kelp + under2 + (1|year_factor) + (1|site), 
                  family = binomial, 
                  weights = fish.area.weight,
+                 na.action = 'na.fail',
                  data = dfx)
 
-# models with kelp summed ####
+m_MNPu <- glmer( TOTyoy_pres ~  Macro+Nereo+under2+ (1|year_factor) + (1|site), 
+                 family = binomial, 
+                 weights = fish.area.weight,
+                 na.action = 'na.fail',
+                 data = dfx)
 
-m_three <- glmer( TOTyoy_pres ~ three_kelps+ (1|year_factor) + (1|site), 
-                  family = binomial, 
-                  weights = fish.area.weight,
-                  data = dfx)
+# run model selection
+library(MuMIn)
+
+dd1 <- MuMIn::dredge(m_MNPU)
+dd2 <- MuMIn::dredge(m_CU)
+dd3 <- MuMIn::dredge(m_MNPu , subset = ~under2)
+
+dd = dd1 %>% full_join(.,dd2) %>% full_join(.,dd3)
+
+dd = dd[, c("(Intercept)","Macro","Nereo","Ptery","understory",
+            "canopy_kelp","under2", 
+            "df","logLik","AICc","delta")]
+dd$delta = dd$AICc - min(dd$AICc)
+dd_occur = dd[ order(dd$delta) , ]
+head(dd_occur)
+write.csv(dd_occur, paste0(Fig_Loc,"Fish-YOY-Kelp-binomial-AICc-table.csv"))
+
+
+m_MN <- glmer( TOTyoy_pres ~  Macro+Nereo+ (1|year_factor) + (1|site), 
+                 family = binomial, 
+                 weights = fish.area.weight,
+                 na.action = 'na.fail',
+                 data = dfx)
+
+capture.output(summary(m_MN), file = paste(Results_Loc,"Model-MN-occurence.txt"))
 
 m_canopy <- glmer( TOTyoy_pres ~ canopy_kelp + (1|year_factor) + (1|site), 
-                    family = binomial, 
-                    weights = fish.area.weight,
-                    data = dfx)
-
-# indiv kelps ####
-
-m_nereo <- glmer( TOTyoy_pres ~ Nereo +(1|year_factor) + (1|site), 
-                  family = binomial, 
-                  weights = fish.area.weight,
-                  data = dfx)
-m_macro <- glmer( TOTyoy_pres ~  Macro +  (1|year_factor) + (1|site), 
-                  family = binomial, 
-                  weights = fish.area.weight,
-                  data = dfx)
-m_ptery <- glmer( TOTyoy_pres ~  Ptery +  (1|year_factor) + (1|site), 
-                  family = binomial, 
-                  weights = fish.area.weight,
-                  data = dfx)
-
-# two kelps ####
-
-m_MN <- glmer( TOTyoy_pres ~ Nereo + Macro + (1|year_factor) + (1|site), 
                family = binomial, 
                weights = fish.area.weight,
+               na.action = 'na.fail',
                data = dfx)
 
-m_MP <- glmer( TOTyoy_pres ~  Macro + Ptery+  (1|year_factor) + (1|site), 
-               family = binomial, 
-               weights = fish.area.weight,
-               data = dfx)
-m_NP <- glmer( TOTyoy_pres ~  Nereo + Ptery +  (1|year_factor) + (1|site), 
-               family = binomial, 
-               weights = fish.area.weight,
-               data = dfx)
-
-# three kelps ####
-
-m_MNP <- glmer( TOTyoy_pres ~  Macro+Nereo+Ptery + (1|year_factor) + (1|site), 
-                 family = binomial, 
-                 weights = fish.area.weight,
-                 data = dfx)
-# AIC table
-
-x = ls()
-y = grep("m_",x)
-z = x[y]
-z = z[z != "Sum_Stats"]
-
-
-for(i in 1:length(z)){
-        x = get(z[i])
-        aicc = AICcmodavg::AICc(x)
-        aic = AIC(x)
-        sx = summary(x)
-        npar = nrow(sx$coefficients)
-        mod.name = z[i]
-        x2 = data.frame(cbind(mod.name,aicc,aic, npar))
-        if(i == 1){aic_table <- x2}else{aic_table = rbind(aic_table,x2)}
-}
-
-colnames(aic_table) = c('Model','AICc', 'AIC','Parms')
-aic_table$AICc = as.numeric(aic_table$AICc)
-aic_table$d_AICc <- aic_table$AICc - min(aic_table$AICc)
-aic_table$AIC = as.numeric(aic_table$AIC)
-aic_table$d_AIC <- aic_table$AIC - min(aic_table$AIC)
-# order table by AICc or AIC. Set here
-aic_table = aic_table[order(aic_table$d_AICc),]
-aic_table_occur <- aic_table
-aic_table_occur
-rm(aic_table)
-
-write.csv(aic_table_occur, paste0(Fig_Loc,"Fish-YOY-Kelp-binomial-AICc-table.csv"))
-
-summary(m_canopy)
-summary(m_MN)
+capture.output(summary(m_canopy), file = paste(Results_Loc,"Model-canopy-occurence.txt"))
 
 ##############################################################
 ###### end occurrence models #################################
@@ -237,13 +205,13 @@ dfx$pch = ifelse(dfx$zone==5,21,19)
 
 # plot occurence
 plot_occur_1 <-
-     ggplot( dfx , aes(x=three_kelps,y=p_occur, color = site))+
+     ggplot( dfx , aes(x=canopy_kelp,y=p_occur, color = site))+
      geom_point(pch = 19)+ # dfx$pch)+
      xlab( expression(paste('Canopy kelp stipes per ', m^2)) )+
-     geom_point(aes(x=three_kelps, y = TOTyoy_pres), color='black') + 
+     geom_point(aes(x=canopy_kelp, y = TOTyoy_pres), color='black') + 
      ylab('Probability of occurence') + 
      geom_smooth(formula = y ~ x,
-                 aes(x=three_kelps,y = TOTyoy_pres, group=1),
+                 aes(x=canopy_kelp,y = TOTyoy_pres, group=1),
                  method = "glm", 
                  method.args = list(family = "binomial")) +
      scale_color_manual(values = site.col$col) +
@@ -261,6 +229,7 @@ plot_occur_1 + theme(legend.position = c(0.8,0.3),
 dev.off()
 
 
+
 #####################################################################
 #### begin abundance models ###############################################
 #####################################################################
@@ -269,91 +238,49 @@ dfa = dfx[dfx$TOTyoy_pres == 1,]
 
 dfa$TOTyoy  = log(dfa$TOTyoy)
 
- # dfa$fish.area.weight = 1
 
-a_year <- lmer( TOTyoy ~  (1|year_factor), 
+a_MNPU <- lmer( TOTyoy ~  Macro+Nereo+Ptery+understory + (1|year_factor) + (1|site), 
                 weights = fish.area.weight,
+                 na.action = 'na.fail',
                  data = dfa)
-a_site <- lmer( TOTyoy ~  (1|site), 
-                weights = fish.area.weight,
-                 data = dfa)
-a_yearsite <- lmer( TOTyoy ~  (1|year_factor)+ (1|site), 
-                    weights = fish.area.weight,
-                     data = dfa)
-# summed kelps ####
-
-a_canopy <- lmer( TOTyoy ~ canopy_kelp +(1|year_factor) + (1|site), 
-                  weights = fish.area.weight,
-                 data = dfa)
-a_three  <- lmer( TOTyoy ~ three_kelps +(1|year_factor) + (1|site), 
-                  weights = fish.area.weight,
-                 data = dfa)
-
-# indiv kelps ####
-a_nereo <- lmer( TOTyoy ~ Nereo +(1|year_factor) + (1|site), 
-                 weights = fish.area.weight,
-                  data = dfa)
-a_macro <- lmer( TOTyoy ~  Macro +  (1|year_factor) + (1|site), 
-                 weights = fish.area.weight,
-                  data = dfa)
-a_ptery <- lmer( TOTyoy ~  Ptery+zone +  (1|year_factor) + (1|site), 
-                 weights = fish.area.weight,
-                  data = dfa)
-
-# two kelps
-
-a_MN <- lmer( TOTyoy ~ Nereo+Macro +(1|year_factor) + (1|site), 
-              weights = fish.area.weight,
-               data = dfa)
-a_MP <- lmer( TOTyoy ~  Macro +Ptery+  (1|year_factor) + (1|site), 
-              weights = fish.area.weight,
-               data = dfa)
-a_NP <- lmer( TOTyoy ~  Nereo+ Ptery+zone +  (1|year_factor) + (1|site), 
-              weights = fish.area.weight,
-               data = dfa)
-
-# all kelps
-
-a_MNP <- lmer( TOTyoy ~  Macro+Nereo+Ptery + (1|year_factor) + (1|site), 
+a_CU <- lmer( TOTyoy ~  canopy_kelp + under2 + (1|year_factor) + (1|site), 
                weights = fish.area.weight,
+               na.action = 'na.fail',
                data = dfa)
 
-# AIC
-x = ls()
-y = grep("a_",x)
-z = x[y]
-z = z[ !( z %in% c("Sum_Stats", "Data_Loc")) ]
+a_MNPu <-lmer( TOTyoy ~  Macro+Nereo+under2+ (1|year_factor) + (1|site), 
+                 weights = fish.area.weight,
+                 na.action = 'na.fail',
+                 data = dfa)
 
-for(i in 1:length(z)){
-        print(i)
-        x = get(z[i])
-        aicc = AICcmodavg::AICc(x)
-        aic = AIC(x)
-        sx = summary(x)
-        npar = nrow(sx$coefficients)
-        mod.name = z[i]
-        x2 = data.frame(cbind(mod.name,aicc,aic, npar))
-        if(i == 1){aic_table <- x2}else{aic_table = rbind(aic_table,x2)}
-}
+# run model selection
+library(MuMIn)
 
-colnames(aic_table) = c('Model','AICc','AIC', 'Parms')
-aic_table$AIC = as.numeric(aic_table$AIC)
-aic_table$AICc = as.numeric(aic_table$AICc)
-aic_table$d_AICc <- aic_table$AICc - min(aic_table$AICc)
-aic_table$d_AIC  <- aic_table$AIC  - min(aic_table$AIC)
-aic_table = aic_table[order(aic_table$d_AIC),]
-aic_table_abund <- aic_table
-aic_table_abund
+dd1 <- MuMIn::dredge(a_MNPU)
+dd2 <- MuMIn::dredge(a_CU)
+dd3 <- MuMIn::dredge(a_MNPu , subset = ~ under2)
+
+dd = dd1 %>% full_join(.,dd2) %>% full_join(.,dd3)
+
+dd = dd[, c("(Intercept)","Macro","Nereo","Ptery","understory",
+            "canopy_kelp","under2", 
+            "df","logLik","AICc","delta")]
+dd$delta = dd$AICc - min(dd$AICc)
+dd = dd[ order(dd$delta) , ]
+dd_abund = dd[-1,]
+head(dd_abund)
 
 
-write.csv(aic_table_abund, paste0(Fig_Loc,"Fish-YOY-Kelp-binomial-abundance.csv"))
+write.csv(dd_abund, paste0(Fig_Loc,"Fish-YOY-Kelp-binomial-abundance.csv"))
 
-summary(a_MNP)
+a_base <- lmer( TOTyoy ~ (1|year_factor) + (1|site), 
+                   weights = fish.area.weight,
+                   na.action = 'na.fail',
+                   data = dfa)
+
+capture.output(summary(a_base), file = paste(Results_Loc,"Model-canopy-occurence.txt"))
 
 
-capture.output(summary(m_canopy), file = paste0(Fig_Loc,"Fish-YOY-Kelp-occurence-canopy-table.txt"))
-capture.output(summary(m_MN), file = paste0(Fig_Loc,"Fish-YOY-Kelp-occurence-MN-table.txt"))
-capture.output(summary(a_MNP), file = paste0(Fig_Loc,"Fish-YOY-Kelp-abundance-table.txt"))
 ################################################################################
 ############ end abundance model ###############################################
 ################################################################################
@@ -364,7 +291,7 @@ capture.output(summary(a_MNP), file = paste0(Fig_Loc,"Fish-YOY-Kelp-abundance-ta
 ################################################################################
 
 dfx$pred_occur_mn <-  predict(m_MN , type = 'response')
-dfa$pred_abund_mn <-  exp(predict(a_year , type = 'response'))
+dfa$pred_abund_mn <-  exp(predict(a_base , type = 'response'))
 
 # plot just occurrence ####
 plot_occur <- 
@@ -435,15 +362,16 @@ legend.title.2 = "Probability of\noccurrence"
 
 
 plot_comb = ggplot(df_comb , aes(x = Macro, y = Nereo), color='black') +
-        geom_point(aes( size = pred_occur_mn, fill = predYOY), shape = 21 , alpha = 0.5 ) +
-        # geom_point(data = filter(df_comb, predYOY == 0), size=1, shape = 19, color='black')+
+        geom_point(aes( fill = predYOY), size = 2,  shape = 21 , alpha = 0.5 ) +
+        #geom_point(aes( size = pred_occur_mn, fill = predYOY), shape = 21 , alpha = 0.5 ) +
         scale_fill_gradient(low = "white", high = "red")+
         scale_x_sqrt(breaks = c(0, 0.5, 1,2,4,6,8,10), minor_breaks=0:10) +
         scale_y_sqrt(breaks = c(0, 0.5, 1,2,4,6,8,10), minor_breaks=0:10) +
         labs(x = expression(paste( italic(Macro),' stipes per ', m^2)),
              y = expression(paste( italic(Nereo),' stipes per ', m^2)),
-             size = legend.title.2,
-             fill = legend.title.1) +
+             fill = legend.title.1,
+             # size = legend.title.2
+             ) +
         theme_bw() + theme_nt
         
 plot_comb 
